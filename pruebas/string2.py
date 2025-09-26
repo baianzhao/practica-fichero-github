98644f0b5fe6d8560ad566ed2b0ccabcd00273fe
baianzhao <baian.zhao@campus.xaloc.org>
2025-09-26 15:28:35 +0200

    Create string2.py

---
+++ pruebas/string2.py
@@ -0,0 +1,10 @@
+num1=False
+a=0 #variable para determinar cuando romper el bucle
+while a == 0:
+    num1=input('introduce un numero')
+    siono=num1.isnumeric()
+    if siono == True:
+        print('si es numero')
+        a=1 #cuando el input es un numero, para de preguntar y se interumpe el bucle
+    elif siono == False:
+        print('no es numero, vuelve a introducir')
