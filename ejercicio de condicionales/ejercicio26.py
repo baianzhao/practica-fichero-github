7ec643f79e63266f64a23cd4af6bf9024227cd4a
baianzhao <baian.zhao@campus.xaloc.org>
2025-10-01 09:07:00 +0200

    Create ejercicio26.py

---
+++ ejercicio de condicionales/ejercicio26.py
@@ -0,0 +1,7 @@
+#Repite el programa número 23 pero en esta ocasión utilizando operadores lógicos.
+let=input('introduce una letra')
+mayus=let.isupper()
+if mayus == True:
+    print('la letra es mayuscula')
+if mayus == False:
+    print('la letra es minuscula')
