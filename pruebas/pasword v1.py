7e3c3596e087dfcb1e04b2a36d4f107a08b373f1
baianzhao <baian.zhao@campus.xaloc.org>
2025-10-21 10:41:22 +0200

    Update pasword v1.py

--- pruebas/pasword v1.py
+++ pruebas/pasword v1.py
@@ -13,37 +13,99 @@ print('     Posició 8 Un número menor o igual que 5')
 #asignar el valor de la contraseña
 password=input('Introduce una palabra clave')
 num_err=0
+merror=''
 #detectar el numero de letras
 password_lenth=len(password)
 if password_lenth >= 6 and password_lenth <= 8:
-    if password_lenth == 6:
-        p1=password[0]
-        p2=password[1]
-        p3=password[2]
-        p4=password[3]
-        p5=password[4]
-        p6=password[5]
-        if p1 != 1 and p1 != 5 and p1 > 5:
-            print('Error en el caràcter 1')
-            num_err=num_err +1
-        else:
-            uplow=p2.isupper
-            if uplow == False:
-                print('Error en el caràcter 2')
-                num_err= num_err +1
-            else:
-                print()
-        
-
-
-    elif password_lenth == 7:
-        print('9')
-    
-    elif password_lenth == 8:
-        print('f')
-
+    num_err=num_err
+    merror=merror
 else:
-    print('Error, el password té una longitud de ',password_lenth,' caràcters i no compleix els requisits')#mensaje de error
+    num_err=num_err+1
+    merror=merror+f'/Error, el password té una longitud de {password_lenth} caràcters i no compleix els requisits'
+    print(merror)
+#comprovar caracter 1
+n1=int(password[0])
+if n1 > 5 or n1 < 1:
+    num_err=num_err+1
+    merror=merror+f'/Error, en el caracter 1'
+else:
+    num_err=num_err
+    merror=merror
+#comprovar caracter 2
+n2=password[1]
+if n2.islower() == False:
+    num_err=num_err +1
+    merror=merror+f'/Error, en el caracter 2'
+else:
+    num_err=num_err
+    merror=merror
 
-    
+#comprovar caracter 3
+n3=password[2]
+if n3.isupper() == False:
+    num_err=num_err +1
+    merror=merror+f'/Error, en el caracter 3'
+else:
+    num_err=num_err
+    merror=merror
+#comprovar caracter 4
+if password[3] == '*' or password[3] == '_' or password[3] == '@':
+    num_err=num_err
+    merror=merror
+else:
+    num_err=num_err +1
+    merror=merror+f'/Error, en el caracter 4'
+#comprovar caracter 5
+n4=password[1]
+if n4.islower() == False:
+    num_err=num_err +1
+    merror=merror+f'/Error, en el caracter 5'
+else:
+    num_err=num_err
+    merror=merror
+#comprovar caracter 6
+if password_lenth != 6:
+    num_err=num_err
+else:
+    n6=int(password[5])
+if n6 > 9 or n6 < 6:
+    num_err=num_err+1
+    merror=merror+f'/Error, en el caracter 6'
+#comprovar caracter 7
+if password_lenth==6:
+    num_err=num_err
+    merror=merror
+elif password_lenth == 7:
+    if password[6] == '&' or password[6] == '/' or password[6] == '#':
+        num_err=num_err
+        merror=merror
+    else:
+        num_err=num_err +1
+        merror=merror+f'/Error, en el caracter 7'
+
+elif password_lenth == 8:
+    if password[6] == '&' or password[6] == '/' or password[6] == '#':
+        num_err=num_err
+        merror=merror
+    else:
+        num_err=num_err +1
+        merror=merror+f'/Error, en el caracter 7'
+#comprovar caracter8
+if password_lenth==6:
+    num_err=num_err
+    merror=merror
+elif password_lenth==7:
+    num_err=num_err
+    merror=merror
+elif password_lenth == 8:
+    if password[7] <= 5:
+        num_err=num_err
+        merror=merror
+    else:
+        num_err=num_err +1
+        merror=merror+f'/Error, en el caracter 8'
+
+#mostrar el resultado
+print('el numero total de errores es: ', num_err)
+print(merror)
 
