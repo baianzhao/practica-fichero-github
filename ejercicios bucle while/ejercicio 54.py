2320c03624a2e17607e39154161763eaf49549ee
baianzhao <baian.zhao@campus.xaloc.org>
2025-11-18 10:23:29 +0100

    g

---
+++ ejercicios bucle while/ejercicio 54.py
@@ -0,0 +1,24 @@
+#. Modifica el programa anterior y haz que se repita el ciclo automáticamente hasta que el total 
+#de todas las sumas sea superior a 50, será entonces cuando el programa finalice. No hará falta 
+#preguntar si deseas repetir la operación. En cada operación aparece por pantalla la suma de la 
+#operación y su acumulado. Para aquellos de vosotros que os fijáis en los detalles, controlar que el 
+#mensaje del acumulado es singular o plural.. . Con While
+
+
+si=True
+tts=0
+v=0
+while si== True:
+    n1=int(input('introduce un numero'))
+    n2=int(input('introduce un numero'))
+    total=n1+n2
+    tts=tts+total
+    print(f'el resultado de la suma es: {n1+n2}')
+    
+    if tts>50:
+        si=False
+    elif tts<50:
+        si=True
+        v+=1
+print('programa finalizado')
+print(f'el total de la suma es: {tts} y en total se ha repetido {v} veces')
