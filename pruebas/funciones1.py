57dd15ccadd1a2fe4b8ba2a6be2ddc44e16b42d5
baianzhao <baian.zhao@campus.xaloc.org>
2026-01-30 16:01:04 +0100

    dd

---
+++ pruebas/funciones1.py
@@ -0,0 +1,13 @@
+def de_g_a_kg(num):
+    return num/1000
+print(de_g_a_kg(num=100), 'kilogramos')
+
+
+def funcion1(name):
+    print('hola,',name)
+
+funcion1(name='pepe')
+funcion1(name='juan')
+funcion1(name='jordi')
+funcion1(name='pau')
+funcion1(name='rata')
