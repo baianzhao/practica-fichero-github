dc7735f2b061cff49276abb1779af293bd71fbb1
baianzhao <baian.zhao@campus.xaloc.org>
2026-02-10 10:30:20 +0100

    R

---
+++ jutje ejercicios/P92351.py
@@ -0,0 +1,7 @@
+n=input()
+d,dv=n.split()
+d=int(d)
+dv=int(dv)
+quo=d//dv
+r=d%dv
+print(int(quo),int(r))
---
+++ jutje ejercicios/P98960.py
@@ -0,0 +1,5 @@
+l=input()
+if l.isupper()==True:
+    print(l.lower())
+else:
+    print(l.upper())
