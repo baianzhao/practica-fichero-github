b8448620d4d92d00ec265a010a7716edc4dd769f
baianzhao <baian.zhao@campus.xaloc.org>
2026-02-20 15:42:05 +0100

    r

--- juego ahorcado/ahorcadov1.py
+++ juego ahorcado/ahorcadov1.py
@@ -1,6 +1,6 @@
 import random
 sino=False
-Lista_palabrasecreta=['perro','gato','agua','tiburon','manzana','reloj','ordenador','casa','python','minecraft']
+Lista_palabrasecreta=['perro','gato','agua','tiburon','manzana','reloj','ordenador','casa','python','minecraft','tetris','esternocleidomastoideo']
 Lista_partida=[]
 Lista_ahorcado=[]
 palabra_sec=random.choice(Lista_palabrasecreta)
@@ -13,68 +13,28 @@ for x in range(n_guiones):
     Lista_partida.append(',')
 Lista_partida.pop()
 print('[',*Lista_partida,']')
+ac=0
 err=0
+while sino==False:
+    p=-2
+    list_pos=[]
+    inp=input()
+    inp=inp.lower()
+    if inp in pal_sec_splited:
+        for x in pal_sec_splited:
+            p+=2
+            if inp==x:
+                Lista_partida[p]=inp
+                ac+=1
+        print('[',*Lista_partida,']')
+    if ac==len(pal_sec_splited):
+        break
 
+    else:
+        Lista_ahorcado.append(pal_ahorc[err])
+        err+=1
+        print(*Lista_ahorcado)
+        print('[',*Lista_partida,']')
+        if err==8:
+            break
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#while sino==False:
-#    list_pos=[]
-#    inp=input()
-#    inp=inp.lower()
-#    if inp in pal_sec_splited:
-#        for x in pal_sec_splited:
-#             if x==inp:
-#                pos=pal_sec_splited.index(x)
-#                list_pos.append(pos)
-#                for y in list_pos:
-#                    Lista_partida[pos]==inp
-#            
-#
-#        print('[',*Lista_partida,']')
-#    else:
-#        Lista_ahorcado.append(pal_ahorc[err])
-#        err+=1
-#        print(*Lista_ahorcado)
-#        print('[',*Lista_partida,']')
-#        if err==8:
-#            break
-#print(pos)
-#print(Lista_partida)
