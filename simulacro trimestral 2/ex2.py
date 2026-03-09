77140c8e6d27b6617ee5fb2f8f8771b8520f2d6a
baianzhao <baian.zhao@campus.xaloc.org>
2026-03-09 17:23:33 +0100

    Update ex2.py

--- simulacro trimestral 2/ex2.py
+++ simulacro trimestral 2/ex2.py
@@ -1,30 +1,33 @@
-a,b,c=input().split('-')
-cs=[]
-ci=[]
-cinv=[]
-ea,ea2,na,la=0,0,0,0
-eb=0
-ec=0
-
-for x in a:
-    if x.isnumeric()==False and x.isalpha()==False:
-        ea+=1
-for y in a:
-    if x.isnumeric()==True:
-        na+=1
-for z in a:
-    if x.isalpha()==True:
-        la+=1
-
-if len(a)<8:
-    ea2+=1
-if ea==0 and ea2==0 and na>0 and la>0:
-    cs.append(a)
-elif ea2>0:
-    ci.append(a)
-elif na<=0 or la<=0:
-    ci.append(a)
-elif ea>0:
-    cinv.append(a)
-    
-print(cs,ci,cinv)
+inp=input().split('-')
+print(inp)
+cs,ci,cinv=[],[],[]
+for x in inp:
+    num=0
+    alfa=0
+    inv=0
+    inclen=0
+    splited=[ch for ch in x]
+    if len(splited)<8:
+        inclen=1
+    for y in splited:
+        if y.isalpha()==True:
+            alfa+=1
+        elif y.isnumeric()==True:
+            num+=1
+        elif y.isalpha()==False and y.isnumeric()==False:
+            inv=1
+    if inv!=0:
+        cinv.append(x)
+    elif inclen==1:
+        ci.append(x)
+    elif num>0 and alfa==0:
+        ci.append(x)
+    elif alfa>0 and num==0:
+        ci.append(x)
+    elif inv==0 and num>0 and alfa>0 and inclen==0 and inv==0:
+        cs.append(x)
+maxlen=max(inp, key=len)
+print(cs)
+print(ci)
+print(cinv)
+print(maxlen)
